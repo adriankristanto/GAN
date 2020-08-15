221919ca58d5ed445f1b25b5c1231924df75665f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-15 17:47:38 +1000

    fixed the parameters of Generator class

--- code/models/GAN.py
+++ code/models/GAN.py
@@ -5,17 +5,21 @@ import torch.nn.functional as F
 
 class Generator(nn.Module):
 
-    def __init__(self, layers_dim, activation_func):
+    def __init__(self, layers_dim, internal_activation, output_activation):
         super(Generator, self).__init__()
-        self.layers = self._build(layers_dim, activation_func)
+        self.layers = self._build(layers_dim, internal_activation, output_activation)
 
-    def _build(self, layers_dim, activation_func):
+    def _build(self, layers_dim, internal_activation, output_activation):
         layers = []
         # the first item in layers_dim would be the latent space dimension
         for i in range(1, len(layers_dim)):
             layer = nn.Linear(layers_dim[i-1], layers_dim[i])
             layers.append(layer)
-            layers.append(activation_func)
+            # append internal activation if this is not the last layer, otherwise, append the 
+            # output activation
+            # often, the output layer uses tanh activation function to normalise the image
+            # from [0, 255] to [-1, 1]
+            layers.append(internal_activation if i < len(layers_dim) - 1 else output_activation)
         return nn.Sequential(*layers)
     
     def forward(self, x):
