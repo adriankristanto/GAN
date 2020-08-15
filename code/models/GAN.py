eb1bbf01513db61085019bbe04156187a8df7b57
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-15 17:24:21 +1000

    added the Discriminator class

--- code/models/GAN.py
+++ code/models/GAN.py
@@ -21,3 +21,23 @@ class Generator(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, layers_dim, internal_activation, output_activation):
+        super(Discriminator, self).__init__()
+        self.layers = self._build(layers_dim, internal_activation, output_activation)
+    
+    def _build(self, layers_dim, internal_activation, output_activation):
+        layers = []
+        for i in range(1, len(layers_dim)):
+            layer = nn.Linear(layers_dim[i-1], layers_dim[i])
+            layers.append(layer)
+            # append internal activation if this is not the last layer, otherwise, append the 
+            # output activation
+            layers.append(internal_activation if i < len(layers_dim) - 1 else output_activation)
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
