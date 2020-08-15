0723d638d0b4d1fe53d51ade7456a160e8560d71
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-15 18:20:56 +1000

    instantiate the model

--- code/main.py
+++ code/main.py
@@ -36,3 +36,29 @@ Total training data: {len(trainset)}
 Total testing data: {len(testset)}
 Total data: {len(trainset) + len(testset)}
 """, flush=True)
+
+# 2. instantiate the network model
+Z_DIM = 100
+
+net = GAN(
+    # the latent space dimension is 100
+    g_dims=[Z_DIM, 256, 400, 784],
+    g_in_activation=nn.LeakyReLU(),
+    g_out_activation=nn.Tanh(),
+    # the final layer for the discriminator is just a neuron that returns [0, 1]
+    d_dims=[784, 400, 256, 1],
+    d_in_activation=nn.LeakyReLU(),
+    d_out_activation=nn.Sigmoid()
+)
+
+# if we train with multiple GPUs, we need to use 
+# net.module.generate()
+# with only one GPU, we can simply use
+# net.generate()
+multigpu = False
+if torch.cuda.device_count() > 1:
+    print(f'Number of GPUs: {torch.cuda.device_count()}\n', flush=True)
+    net = nn.DataParallel(net)
+    multigpu = True
+
+net.to(device)
