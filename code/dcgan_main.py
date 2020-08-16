b8d4ea053b012d9211b8cad7d00f52b0ae4cefc7
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-16 18:58:04 +1000

    added the training script for dcgan & load the data

---
+++ code/dcgan_main.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tqdm import tqdm
+import os
+import models.DCGAN as DCGAN
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Current device: {device}', flush=True)
+
+# 1. load the data
+DATA_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../data/'
+BATCH_SIZE = 128
+
+train_transform = transforms.Compose([
+    transforms.ToTensor(),
+    # each pixel of each image from the MNIST dataset is within the range [0, 1]
+    # normalizing with mean=0.5 and std=0.5
+    # 0 <= x <= 1
+    # -0.5 <= x - 0.5 <= 0.5
+    # -1 <= (x - 0.5) / 0.5 <= 1
+    # thus, changing its range to [-1, 1]
+    # this is because the generator uses tanh on the output layer, which has the range [-1, 1]
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+trainset = datasets.MNIST(root=DATA_PATH, train=True, transform=train_transform, download=True)
+testset = datasets.MNIST(root=DATA_PATH, train=False, transform=test_transform, download=True)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
+
+print(f"""
+Total training data: {len(trainset)}
+Total testing data: {len(testset)}
+Total data: {len(trainset) + len(testset)}
+""", flush=True)
+
