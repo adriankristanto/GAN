78ab742f7a02f355fe8873ffd5f65ff9aef6175f
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-17 18:16:52 +1000

    added the class definition of WGANGP Critic

--- code/models/WGANGP.py
+++ code/models/WGANGP.py
@@ -43,6 +43,41 @@ class Generator(nn.Module):
         x = self.layers(x)
         return x
 
+class Critic(nn.Module):
+
+    # note that we don't use batchnorm in the critic
+    def __init__(self, channels, kernels, strides, paddings, internal_activation):
+        super(Critic, self).__init__()
+        self.layers = self._build(channels, kernels, strides, paddings, internal_activation)
+        self._init_weights()
+    
+    def _build(self, channels, kernels, strides, paddings, internal_activation):
+        layers = []
+        for i in range(1, len(channels)):
+            layer = nn.Conv2d(
+                in_channels=channels[i-1],
+                out_channels=channels[i],
+                kernel_size=kernels[i],
+                stride=strides[i],
+                padding=paddings[i],
+            )
+            layers.append(layer)
+            # add activation function when it is not the final layer
+            # the output layer doesn't need any activation
+            if i < len(channels) - 1:
+                layers.append(internal_activation)
+        return nn.Sequential(*layers)
+    
+    def _init_weights(self):
+        # no batch norm layer
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.normal_(module.weight, mean=0.0, std=0.02)
+    
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+
 
 if __name__ == "__main__":
     # Generator G
@@ -59,3 +94,14 @@ if __name__ == "__main__":
     print(G)
     sample = torch.randn((1, Z_DIM, 1, 1))
     print(G(sample).shape)
+
+    # Critic D
+    D = Critic(
+        channels=[1, 64, 128, 256, 1],
+        kernels=[None, 4, 4, 5, 7],
+        strides=[None, 2, 2, 1, 1],
+        paddings=[None, 1, 1, 2, 0],
+        internal_activation=nn.LeakyReLU(0.2),
+    )
+    print(D)
+    print(D(G(sample)).shape)
